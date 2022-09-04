7e129b0ee7da0e9012a6818a8113cf129817e021
Vinicius Holub <vostron@halfgenius.network>
2022-09-04 18:58:28 -0300

    Exercício 4

---
+++ src/Aula02/Ex04.java
@@ -0,0 +1,22 @@
+package Aula02;
+
+import java.util.Scanner;
+
+public class Ex04 {
+	public static void main(String [] args) {
+		Scanner ler = new Scanner(System.in);
+		double[] v = {0, 0, 0, 0};
+		double sum = 0;
+		
+		for(int i = 0; i < 4; i++) {
+			System.out.printf("Insira o %d valor: ", (i+1));
+			v[i] = ler.nextDouble();
+		}
+		
+		for(int i = 0; i < 4; i++)
+			sum += v[i];
+		
+		System.out.printf("A media dos valores digitados e %.2f", sum/4);
+		
+	}
+}
