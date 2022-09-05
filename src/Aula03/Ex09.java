2877e73cc3cda716cdf32fc8dd6eeaf645670fe1
Vinicius Holub <vostron@halfgenius.network>
2022-09-04 21:46:21 -0300

    Exercício 9

---
+++ src/Aula03/Ex09.java
@@ -0,0 +1,22 @@
+package Aula03;
+
+import java.util.Scanner;
+
+public class Ex09 {
+	public static void main(String [] args) {
+		Scanner ler = new Scanner(System.in);
+		float v1, v2;
+		
+		System.out.printf("Informe o 1o numero: ");
+		v1 = ler.nextFloat();
+		
+		System.out.printf("Informe o 2o numero: ");
+		v2 = ler.nextFloat();
+		
+		if(v1 < v2)
+			System.out.printf("O menor valor e %.2f", v1);
+		
+		if(v2 < v1)
+			System.out.printf("O menor valor e %.2f", v2);
+	}
+}
