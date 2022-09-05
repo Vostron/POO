bcd485e9b2912ac5e24e1c8bc3eb21deb653164b
Vinicius Holub <vostron@halfgenius.network>
2022-09-04 21:47:23 -0300

    Exercício 18

---
+++ src/Aula03/Ex18.java
@@ -0,0 +1,10 @@
+package Aula03;
+
+import java.util.Scanner;
+
+public class Ex18 {
+	//import java.util.Scanner;
+	public static void main(String [] args) {
+		Scanner ler = new Scanner(System.in);
+	}
+}
