cdc82677821f0930ea8492fde249522cd812013c
Vinicius Holub <vostron@halfgenius.network>
2022-09-04 21:47:07 -0300

    Exercício 16

---
+++ src/Aula03/Ex16.java
@@ -0,0 +1,10 @@
+package Aula03;
+
+import java.util.Scanner;
+
+public class Ex16 {
+	//import java.util.Scanner;
+	public static void main(String [] args) {
+		Scanner ler = new Scanner(System.in);
+	}
+}
