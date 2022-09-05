e19638c7895bf2162da0b089b4bf378a337a0353
Vinicius Holub <vostron@halfgenius.network>
2022-09-04 21:48:32 -0300

    Exercício 28

---
+++ src/Aula03/Ex28.java
@@ -0,0 +1,10 @@
+package Aula03;
+
+import java.util.Scanner;
+
+public class Ex28 {
+	//import java.util.Scanner;
+	public static void main(String [] args) {
+		Scanner ler = new Scanner(System.in);
+	}
+}
