ce4ad99ef1aafc090accf04aebe73ec8bf8a2319
Vinicius Holub <vostron@halfgenius.network>
2022-09-04 21:46:35 -0300

    Exercício 11

---
+++ src/Aula03/Ex11.java
@@ -0,0 +1,23 @@
+package Aula03;
+
+import java.util.Scanner;
+
+public class Ex11 {
+	public static void main(String [] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		float b, a;
+		
+		System.out.printf("Informe o tamanho da base (cm): ");
+		b = ler.nextFloat();
+		
+		System.out.printf("Informe o tamanho da altura (cm): ");
+		a = ler.nextFloat();
+		
+		System.out.printf("Tamanho da area: %.2f cm quadrados", (b*a));
+		
+		if((b*a) > 100)
+			System.out.printf("\nTerreno grande");
+		
+	}
+}
