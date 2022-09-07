2616b7eba599088a392100d8b92196999e7546f7
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 20:28:00 -0300

    Update Ex27

--- src/Aula03/Ex27.java
+++ src/Aula03/Ex27.java
@@ -3,8 +3,17 @@ package Aula03;
 import java.util.Scanner;
 
 public class Ex27 {
-	//import java.util.Scanner;
 	public static void main(String [] args) {
 		Scanner ler = new Scanner(System.in);
+		
+		float x;
+		
+		System.out.printf("Digite um numero: ");
+		x = ler.nextFloat();
+		
+		if(x % 2 == 0)
+			System.out.printf("%.2f + 5 = %.2f", x, x + 5);
+		else
+			System.out.printf("%.2f + 8 = %.2f", x, x + 8);
 	}
 }
