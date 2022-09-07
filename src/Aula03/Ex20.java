c8796ed4dbf8682b54bf106769d1c91af0f5e06a
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 19:20:51 -0300

    Update Ex20

--- src/Aula03/Ex20.java
+++ src/Aula03/Ex20.java
@@ -3,8 +3,16 @@ package Aula03;
 import java.util.Scanner;
 
 public class Ex20 {
-	//import java.util.Scanner;
 	public static void main(String [] args) {
 		Scanner ler = new Scanner(System.in);
+		
+		float p1, p2;
+		
+		System.out.printf("Informe a nota P1: ");
+		p1 = ler.nextFloat();
+		
+		p2 = ((5*3) - p1)/2;
+		
+		System.out.printf("Voce precisa obter nota %.2f na P2 para ser aprovado.", p2);
 	}
 }
