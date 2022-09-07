53eb533eb400651cfd96269c7b30a8fe4c77a58c
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 20:25:53 -0300

    Update Ex26

--- src/Aula03/Ex26.java
+++ src/Aula03/Ex26.java
@@ -3,8 +3,17 @@ package Aula03;
 import java.util.Scanner;
 
 public class Ex26 {
-	//import java.util.Scanner;
 	public static void main(String [] args) {
 		Scanner ler = new Scanner(System.in);
+		
+		float x;
+		
+		System.out.printf("Digite um numero: ");
+		x = ler.nextFloat();
+		
+		if(x >= 0)
+			System.out.printf("O dobro de %.2f e %.2f", x, x*2);
+		else
+			System.out.printf("O triplo de %.2f e %.2f", x, x*3);
 	}
 }
