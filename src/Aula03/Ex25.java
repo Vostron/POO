d64e2c07ab00728b03c79bce52ab864899161bbd
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 20:22:53 -0300

    Update Ex25

--- src/Aula03/Ex25.java
+++ src/Aula03/Ex25.java
@@ -3,8 +3,17 @@ package Aula03;
 import java.util.Scanner;
 
 public class Ex25 {
-	//import java.util.Scanner;
 	public static void main(String [] args) {
 		Scanner ler = new Scanner(System.in);
+		
+		float x;
+		
+		System.out.printf("Digite um numero: ");
+		x = ler.nextFloat();
+		
+		if(x % 2 == 0)
+			System.out.printf("O numero digitado e par.");
+		else
+			System.out.printf("O numero digitado e impar.");
 	}
 }
