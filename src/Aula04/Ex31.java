7bd0fbe695ff0310207def3caf8b247a3cdad660
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 21:13:26 -0300

    Update Ex31

--- src/Aula04/Ex31.java
+++ src/Aula04/Ex31.java
@@ -1,5 +1,18 @@
 package Aula04;
 
-public class Ex31 {
+import java.util.Scanner;
 
+public class Ex31 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		float i;
+		
+		do {
+			System.out.printf("Digite um numero positivo: ");
+			i = ler.nextFloat();
+		}while(i >= 0);
+		
+		System.out.printf("Voce digitou um numero negativo");
+		
+	}
 }
