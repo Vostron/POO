9b4b63b3d62faf98e469df5e5420ca80c4cf1956
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 21:42:13 -0300

    Update Ex37

--- src/Aula04/Ex37.java
+++ src/Aula04/Ex37.java
@@ -1,5 +1,23 @@
 package Aula04;
 
-public class Ex37 {
+import java.util.Scanner;
 
+public class Ex37 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		for(int i = 1; i < 21; i++) {
+			System.out.printf(""
+					+ "\n#################\n"
+					+ "#  Tabuada %-2d   #\n"
+					+ "#################\n", i);			
+			
+			for(int j = 1; j < 11; j++)
+				System.out.printf("# %-2d x %-2d = %-3d #\n", j, i, (i * j));
+			System.out.printf("#################\n");
+			
+			System.out.printf("Pressione qualquer tecla para continuar...");
+			ler.nextLine();
+		}
+	}
 }
