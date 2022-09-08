f86c5b8537cf3022ef434bb5645b2a7510f1076d
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 23:14:23 -0300

    Update Ex41

--- src/Aula04/Ex41.java
+++ src/Aula04/Ex41.java
@@ -1,5 +1,21 @@
 package Aula04;
 
-public class Ex41 {
+import java.util.Scanner;
 
+public class Ex41 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int j = 1;
+		int[] v = new int[30];
+		for(int i = 0; i < 30; i++) {
+			if(i == 0) {
+				v[i] = i + j;
+				continue;
+			}
+			v[i] = v[i-1] + j;
+			j += 2;
+			System.out.printf("%d\n", v[i]);
+		}
+		
+	}
 }
