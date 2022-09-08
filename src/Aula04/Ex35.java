ac1ef842653aa9aa0af17ed9420c7642d880dee1
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 21:27:20 -0300

    Update Ex35

--- src/Aula04/Ex35.java
+++ src/Aula04/Ex35.java
@@ -1,5 +1,19 @@
 package Aula04;
 
-public class Ex35 {
+import java.util.Scanner;
 
+public class Ex35 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int a;
+		
+		do {
+			System.out.printf("Digite um numero positivo: ");
+			a = ler.nextInt();
+		}while(a <= 0);
+		
+		for(int i = 0; i < 10; i++) 
+			System.out.printf("%d x %d = %d\n", (i+1), a, (i+1)*a);
+		
+	}
 }
