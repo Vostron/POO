aadb606fd2b2dee71a96a444f1977989f092d3f9
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 23:15:34 -0300

    Update Ex40

--- src/Aula04/Ex40.java
+++ src/Aula04/Ex40.java
@@ -1,11 +1,7 @@
 package Aula04;
 
-import java.util.Scanner;
-
 public class Ex40 {
 	public static void main(String[] args) {
-		Scanner ler = new Scanner(System.in);
-		
 		int[] v = new int[30];
 		int sum = 0;
 		
