7322c6aeeee2d2bcc9eaa7381d04c1a232562e49
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 21:14:41 -0300

    Update Ex33

--- src/Aula04/Ex33.java
+++ src/Aula04/Ex33.java
@@ -1,5 +1,19 @@
 package Aula04;
 
-public class Ex33 {
+import java.util.Scanner;
 
+public class Ex33 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		char sexo;
+		
+		for(;;) {
+			System.out.printf("Digite o sexo do usuario (M ou F): ");
+			sexo = ler.nextLine().charAt(0);
+			
+			if(!(sexo == 'M' || sexo == 'F'))
+				break;		
+		}
+		System.out.printf("Caractere invalido digitado.");
+	}
 }
