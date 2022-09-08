a02f10ac25e90f6e3342bd578015f867d6445de5
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 23:15:29 -0300

    Update Ex39

--- src/Aula04/Ex39.java
+++ src/Aula04/Ex39.java
@@ -1,11 +1,7 @@
 package Aula04;
 
-import java.util.Scanner;
-
 public class Ex39 {
 	public static void main(String[] args) {
-		Scanner ler = new Scanner(System.in);
-		
 		int fib1 = 0, fib2 = -1;
 		
 		for(int i = 0; i < 30; i++) {
