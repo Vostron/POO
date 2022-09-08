ad297a09f8906a7aa33f3c0177586dab86efa416
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 21:13:57 -0300

    Update Ex32

--- src/Aula04/Ex32.java
+++ src/Aula04/Ex32.java
@@ -1,5 +1,21 @@
 package Aula04;
 
-public class Ex32 {
+import java.util.Scanner;
 
+public class Ex32 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		float a, b;
+		
+		System.out.printf("Digite um numero: ");
+		a = ler.nextFloat();
+		
+		do {
+			System.out.printf("Digite outro numero (maior que o primeiro): ");
+			b = ler.nextFloat();		
+		}while(b < a);
+		
+		System.out.printf("Fim do programa.");
+	}
 }
