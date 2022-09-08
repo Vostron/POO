b3eed5eb4b6fe5cc56ac5815ff27be1d699611a7
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 21:27:29 -0300

    Update Ex36

--- src/Aula04/Ex36.java
+++ src/Aula04/Ex36.java
@@ -1,5 +1,33 @@
 package Aula04;
 
-public class Ex36 {
+import java.util.Scanner;
 
+public class Ex36 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int x, intInicio, intFim;
+		
+		do {
+			System.out.printf("Digite um numero positivo: ");
+			x = ler.nextInt();
+			
+			if(x < 0)
+				System.out.printf("Apenas numeros positivos sao aceitos. \n");
+		}while(x < 0);
+		
+		
+		System.out.printf("Informe o inicio do intervalo para calculo da tabuada do numero %d: ", x);
+		intInicio = ler.nextInt();
+		
+		do {
+			System.out.printf("Informe o fim do intervalo para calculo da tabuada do numero %d: ", x);
+			intFim = ler.nextInt();
+			
+			if(intFim <= intInicio)
+				System.out.printf("O valor final do intervalo devera ser maior que o inicial.\n");
+		}while(intFim <= intInicio);
+		
+		for(int i = intInicio; i <= intFim; i++)
+			System.out.printf("%d x %d = %d\n", i, x, (i * x));
+	}
 }
