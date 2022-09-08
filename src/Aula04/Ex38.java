2655f51c1c6d31257734b3ffbf55b8f2e0d94242
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 21:44:54 -0300

    Update Ex38

--- src/Aula04/Ex38.java
+++ src/Aula04/Ex38.java
@@ -1,5 +1,15 @@
 package Aula04;
 
-public class Ex38 {
+import java.util.Scanner;
 
+public class Ex38 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int sum = 0;
+		
+		for(int i = 1; i <= 100; i++)
+			sum += i;
+		
+		System.out.printf("A soma dos inteiros positivos de 1 a 100 e: %d", sum);
+	}
 }
