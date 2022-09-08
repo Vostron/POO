52555b07f0382a2f11e0de331ed7518259764a0e
Vinicius Holub <vostron@halfgenius.network>
2022-09-07 21:15:13 -0300

    Update Ex34

--- src/Aula04/Ex34.java
+++ src/Aula04/Ex34.java
@@ -1,5 +1,9 @@
 package Aula04;
 
 public class Ex34 {
-
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) {
+			System.out.printf("%d x %d = %d\n", (i+1), 5, (i+1)*5);
+		}
+	}
 }
