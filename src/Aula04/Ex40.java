2182012d8d28a777b2ecf61318215de189d10ae5
Vinicius Holub <vostron@halfgenius.network>
2022-09-19 22:49:10 -0300

    Ajuste de sequënciamento

--- src/Aula04/Ex40.java
+++ src/Aula04/Ex40.java
@@ -2,10 +2,10 @@ package Aula04;
 
 public class Ex40 {
 	public static void main(String[] args) {
-		int[] v = new int[30];
+		int[] v = new int[20];
 		int sum = 0;
 		
-		for(int i = 0; i < 30; i++) {
+		for(int i = 0; i < 20; i++) {
 			sum = 0;
 			
 			for(int j = 1; j < 4; j++)
