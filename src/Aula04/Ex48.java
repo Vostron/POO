3c3b58ebdfc5f269bf2ae65a261412a1cbafa5d0
Vinicius Holub <vostron@halfgenius.network>
2022-09-21 17:22:55 -0300

    Update Ex48.java

--- src/Aula04/Ex48.java
+++ src/Aula04/Ex48.java
@@ -1,5 +1,22 @@
 package Aula04;
 
-public class Ex48 {
+import java.util.Scanner;
 
+public class Ex48 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int i;
+		System.out.printf("Digite um numero inteiro qualquer: ");
+		i = ler.nextInt();
+		
+		System.out.printf("Imprimindo os 20 numeros subsequentes: ");
+		for(int j = 0; j < 20; j++) {
+			i++;
+			System.out.printf("%d", i);
+			if(j < 19)
+				System.out.printf(", ");
+			else
+				System.out.printf(".");
+		}
+	}
 }
