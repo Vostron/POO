f2d0be2cdffad70d968d70cd9f0cf6c23c70af11
Vinicius Holub <vostron@halfgenius.network>
2022-09-22 08:34:56 -0300

    Update Ex50.java

--- src/Aula04/Ex50.java
+++ src/Aula04/Ex50.java
@@ -1,5 +1,22 @@
 package Aula04;
 
-public class Ex50 {
+import java.util.Scanner;
 
+public class Ex50 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int a, b;
+		
+		System.out.printf("Digite o inicio do intervalo fechado: ");
+		a = ler.nextInt();
+		
+		System.out.printf("Digite o fim do intervalo fechado: ");
+		b = ler.nextInt();
+		
+		System.out.println("No intervalo digitado, esses sao os numeros pares maiores que 10:");
+		
+		for(int i = a; i < b; i++)
+			if( (i%2 == 0)  && i > 10)
+				System.out.printf("%d, ", i);
+	}
 }
