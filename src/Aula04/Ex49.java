2084812de1ce710c0532768eff1eb9fff1aad17a
Vinicius Holub <vostron@halfgenius.network>
2022-09-22 08:34:53 -0300

    Update Ex49.java

--- src/Aula04/Ex49.java
+++ src/Aula04/Ex49.java
@@ -1,5 +1,24 @@
 package Aula04;
 
-public class Ex49 {
+import java.util.Scanner;
 
+public class Ex49 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int a, b, sum = 0, mem;
+		
+		System.out.printf("Digite o primeiro numero do intervalo (inicio): ");
+		a = ler.nextInt();
+		mem = a;
+		
+		System.out.printf("Digite o segundo numero do intervalo (fim): ");
+		b = ler.nextInt();
+		
+		for(int i = (a - 1); i < b; i++) {
+			sum += a;
+			a++;
+		}
+		
+		System.out.printf("\nA soma dos numeros entre %d e %d e %d", mem, b, sum);
+	}
 }
