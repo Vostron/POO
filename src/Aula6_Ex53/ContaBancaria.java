ebcba2846203389e9ad07c22b47a9d92c0997a34
Vinicius Holub <vostron@halfgenius.network>
2022-09-22 10:18:03 -0300

    Create ContaBancaria.java

---
+++ src/Aula6_Ex53/ContaBancaria.java
@@ -0,0 +1,17 @@
+package Aula6_Ex53;
+
+public class ContaBancaria {
+	public String agencia;
+	public String numero;
+	public double saldo;
+	
+	public ContaBancaria() {
+		
+	}
+	
+	public ContaBancaria(String agencia, String numero, double saldo) {
+		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+}
